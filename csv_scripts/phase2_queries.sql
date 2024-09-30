b695a5ac6ea7d3134b40b3c47a96fe12a7ddf3c8
ryantran <113406848+rryantran@users.noreply.github.com>
2024-09-30 15:48:59 -0500

    add two queries + minor edits

--- csv_scripts/phase2_queries.sql
+++ csv_scripts/phase2_queries.sql
@@ -1,30 +1,32 @@
--- 12 queries total, 3 per person, write comment for the intent of each query-- transactions during a date
-SELECT 'The total amount of transactions during ' || MAX(transaction_date) || ' is: ' || COUNT(*) AS result
+-- 12 queries total, 3 per person, write comment for the intent of each query
+
+-- transactions during a date
+SELECT 'The total number of transactions during ' || transaction_date || ' is: ' || COUNT(transaction_id) AS result
 FROM transaction
-WHERE transaction_date = '2024-01-27';
--- transaction during a specific hour during a specific month
-SELECT 
-    'The total amount of transactions during hour ' || EXTRACT(HOUR FROM transaction_time) || 
-    ' in month ' || EXTRACT(MONTH FROM transaction_date) || 
-    ' is: ' || COUNT(*) AS result
+WHERE transaction_date = '2024-01-27'
+GROUP BY transaction_date;
+
+-- transactions during a specific hour of a specific month
+SELECT 'The total number of transactions during hour ' || EXTRACT(HOUR FROM transaction_time) || ' in month ' || EXTRACT(MONTH FROM transaction_date) || ' is: ' || COUNT(transaction_id) AS result
 FROM transaction
 WHERE EXTRACT(MONTH FROM transaction_date) = 1
 AND EXTRACT(HOUR FROM transaction_time) = 11
 GROUP BY EXTRACT(HOUR FROM transaction_time), EXTRACT(MONTH FROM transaction_date);
+
 -- SPECIAL QUERY 1
-SELECT 'Week ' || MAX(week_number) || ' is: ' || COUNT(*) AS result
+SELECT 'Week ' || week_number || ' had ' || COUNT(transaction_id) || ' transactions' AS result
 FROM transaction
-WHERE week_number = 5;
+WHERE week_number = 5
+GROUP BY week_number;
+
 -- SPECIAL QUERY 2
-SELECT 
-    'The total amount of transactions during hour ' || EXTRACT(HOUR FROM transaction_time) || 
-    ' is: ' || COUNT(*) || ', totaling $' || SUM(total_cost) AS result
+SELECT 'The total number of transactions during hour ' || EXTRACT(HOUR FROM transaction_time) || ' is: ' || COUNT(transaction_id) || ', totaling $' || SUM(total_cost) AS result
 FROM transaction
 WHERE EXTRACT(HOUR FROM transaction_time) = 11
 GROUP BY EXTRACT(HOUR FROM transaction_time);
+
 -- SPECIAL QUERY 3
-SELECT 
-    '2024-01-27 has $' || SUM(total_cost) AS result
+SELECT '2024-01-27 has $' || SUM(total_cost) || " in top sales" AS result
 FROM (
     SELECT total_cost
     FROM transaction
@@ -83,3 +85,17 @@ GROUP BY
 ORDER BY 
     SUM(inventory_menu_item.ingredient_amount * menu_item_transaction.item_quantity) DESC
 LIMIT 8;
+
+--Ryan Tran
+-- inventory items sorted by closest to running out
+SELECT i.ingredient_name, i.current_stock, i.min_stock, i.current_stock - i.min_stock AS difference 
+FROM inventory AS i 
+ORDER BY difference DESC;
+
+-- employee with the most transactions
+SELECT e.employee_id, e.first_name, e.last_name, COUNT(t.transaction_id) AS transaction_count 
+FROM employee AS e 
+INNER JOIN transaction AS t ON e.employee_id = t.employee_id 
+GROUP BY e.employee_id, e.first_name, e.last_name 
+ORDER BY transaction_count DESC
+LIMIT 1;
