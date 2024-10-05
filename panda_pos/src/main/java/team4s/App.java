6b72a8d06ce9839f4691196da809e4c3f5a79b8b
EshwarReddy13 <eshwarreddygadi@gmail.com>
2024-10-05 12:39:43 -0500

    Added two new files Manager and Cashier and connected them to the main App

--- panda_pos/src/main/java/team4s/App.java
+++ panda_pos/src/main/java/team4s/App.java
@@ -1,38 +1,60 @@
 package team4s;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
-
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+    static Stage mainStage;
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+        mainStage = primaryStage;
+        mainStage.setTitle("App");
+
+        createMainMenuScene();
+
+        mainStage.setMaximized(true);
+        mainStage.show();
+    }
+
+    public void createMainMenuScene() {
+        VBox root = new VBox();
+        root.setSpacing(10);
+        root.setAlignment(Pos.CENTER);
+
+        Button man = new Button("Manager");
+        Button cahsier = new Button("Cashier");
+
+        man.setOnAction(e -> openManager());
+        cahsier.setOnAction(e -> openCashier());
+
+        root.getChildren().addAll(man, cahsier);
+        Scene scene = new Scene(root, 400, 400);
+        mainStage.setScene(scene);
+
+        mainStage.setMaximized(true);
+    }
+
+    public void openManager() {
+        Manager managerUI = new Manager();
+        managerUI.start(mainStage);
+
+        mainStage.setMaximized(true);
+    }
+
+    public void openCashier() {
+        Cashier cashierUI = new Cashier();
+        cashierUI.start(mainStage);
+
+        mainStage.setMaximized(true);
+    }
 }
+
